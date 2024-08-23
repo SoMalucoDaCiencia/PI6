27618ea03c08590860e5b53c136cce484f125f87
a.andrade <arthur10andrade@gmail.com.br>
2024-08-23 19:23:11 -0300

    main

--- src/core/main.go
+++ src/core/main.go
@@ -2,23 +2,20 @@ package main
 
 import (
 	"PI6/src"
+	"PI6/src/database"
+	"fmt"
 )
 
 func main() {
 
-	// err := src.Setup()
-	// if err != nil {
-	// 	panic(fmt.Errorf("an error occured while starting application: %v", err.Error()))
-	// }
-
-	// err = database.CheckDatabase()
-	// if err != nil {
-	// 	panic(fmt.Errorf("an error occured while starting application: %v", err.Error()))
-	// }
-
-	err := src.FilterCEPs()
+	err := src.Setup()
 	if err != nil {
-		panic(err)
+		panic(fmt.Errorf("an error occured while starting application: %v", err.Error()))
+	}
+
+	err = database.CheckDatabase()
+	if err != nil {
+		panic(fmt.Errorf("an error occured while starting application: %v", err.Error()))
 	}
 
 	println("done...")
